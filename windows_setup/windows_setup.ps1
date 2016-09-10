6c01bcc7147697ffb23397696fc055d25e6648e0
leunammejii <theemailaddressishare@gmail.com>
2016-09-10 19:45:28 -0400

    Add PowerShell script

---
+++ windows_setup/windows_setup.ps1
@@ -0,0 +1,7 @@
+cd $HOME
+mkdir -p leunammejii\ansible-N-vagrant
+cd leunammejii\ansible-N-vagrant
+git init
+git pull https://github.com/leunammejii/ansible-N-vagrant.git
+cd windows_setup
+vagrant up  
