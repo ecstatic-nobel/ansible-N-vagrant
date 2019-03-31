d7663820f3a90a31f733c2401d509896b022c300
Ecstatic Nobel <12433311+ecstatic-nobel@users.noreply.github.com>
2019-03-30 22:51:28 -0400

    Update folder name

--- windows_setup/windows_setup.ps1
+++ windows_setup/windows_setup.ps1
@@ -1,7 +1,7 @@
 cd $HOME
-mkdir -p leunammejii\ansible-N-vagrant
-cd leunammejii\ansible-N-vagrant
+mkdir -p ecstatic-nobel\ansible-N-vagrant
+cd ecstatic-nobel\ansible-N-vagrant
 git init
-git pull https://github.com/leunammejii/ansible-N-vagrant.git
+git pull https://github.com/ecstatic-nobel/ansible-N-vagrant.git
 cd windows_setup
 vagrant up  
